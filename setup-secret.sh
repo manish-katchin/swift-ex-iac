0382cfa86d575ccc429a2a4fb0155ee2306ec611
manish-katchin <manish@katchintech.com>
2025-11-07 11:41:18 +0530

    added template for creating secret

---
+++ setup-secret.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+[ ! -z ${CI} ] || . ./config.dev
+
+echo $ENVIRONMENT_NAME
+
+STACK_NAME="$ENVIRONMENT_NAME-$COMPONENT_NAME-$PART_NAME-secrets"
+TEMPLATE_FILE="./create-secret.yaml"
+
+echo "Deploying Secret stack: $STACK_NAME"
+
+aws cloudformation deploy \
+    --template-file "$TEMPLATE_FILE" \
+    --stack-name "$STACK_NAME" \
+    --parameter-overrides \
+        StellarADDRESS="$STELLAR_ADDRESS" \
+        Environment="$ENVIRONMENT_NAME" \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --no-fail-on-empty-changeset \
+    --region $AWS_REGION
